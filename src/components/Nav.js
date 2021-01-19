1f6591fa3d52a07d5253ad65b60e2b35146b1080
farhan khan <farhan.alws4u247@gmail.com>
2021-01-19 23:17:14 +0530

    this is the last commit
    app is completed but some things are working like all trailers are not
    playing exept one. and also horizontal scrolling is not working. other
    things are working perfectly and app is looking great.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import "./Nav.css";
+
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 90) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
-      <img className="nav_logo" src="" />
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://download.logo.wine/logo/Netflix/Netflix-Logo.wine.png"
+      />
     </div>
   );
 };
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,22 +1,55 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import axios from "../axios"; // becaue of defualt import instance is renamed to axios here.
 import "./Row.css";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/"; // this is the base url for images of all movies.
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // a snippet of code which runs on a specific condition/variable.
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
-  }, [fetchUrl]); // if we leave the brackets blank we say run the once on page load. if we put movies under brackets like this [fetchUrl]. it will run when fetchUrl changes.
+  }, [fetchUrl]); // if we leave the brackets blank we say run the once on page load. if we put fetchUrl under brackets like this [fetchUrl]. it will run when fetchUrl changes.
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "") // this function (movieTrailer) here will give us a url of trailer like this https://www.youtube.com/watch?   v=6uE4nfFgc5Q.
+        .then((url) => {
+          // and here we will try to extract the movieId from that url which looks like this v=6uE4nfFgc5Q.
+
+          const urlParams = new URLSearchParams(new URL(url).search); // new URL(url).search; this gives everything after the question mark in url. and URLSearchParams will allow us to fetch or make a get request to what we find in  new URL(url).search that is 'v';
+
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.log(
+            "your trailer not found because of this error",
+            error,
+            movie
+          );
+        });
+    }
+  };
 
   return (
     <div className="row">
@@ -25,6 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -33,6 +67,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
+      <div>{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}</div>
     </div>
   );
 }
